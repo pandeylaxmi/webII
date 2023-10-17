9d5ef2cca48312019ebab4b3d8d47ad8859cda95
pandeylaxmi <107383595+pandeylaxmi@users.noreply.github.com>
2023-10-17 11:36:07 +0900

    operator1

---
+++ src/main/kotlin/Operators1.kt
@@ -0,0 +1,26 @@
+fun main() {
+    val add = 6 + 2
+    println(add) // 8
+print("add 6 and 2")
+    val minus = 6 - 3
+    println(minus) // 4
+print("subtract 3 from 6")
+    val mul = 6 * 3
+    println(mul) // 12
+print("multiply 6 and 3")
+    val divide = 6 / 2
+    println(divide) // 3
+print("divide 6 by 2")
+    val divide2 = 7 / 2
+    println(divide2) // 3 (!)
+print("divide7 by 2")
+    val divide3 = 8 / 4
+    println(divide3) // 4
+print("divide 8 by 4")
+    val divide4 = 7 / 2.0
+    println(divide4) // 3.5 (!)
+print("divide7 by 2.0")
+    val remainder = 7 % 3
+    println(remainder) // 1
+    print("divide 7 by 3")
+}
